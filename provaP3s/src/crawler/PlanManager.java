e67c34ef5adaf2783610a32fa6e8f3e93c6e62b8
savmil <sav.milo@studenti.unina.it>
2018-06-30 17:34:45 +0200

    plan Manager

--- provaP3s/src/crawler/PlanManager.java
+++ provaP3s/src/crawler/PlanManager.java
@@ -6,12 +6,29 @@ public class PlanManager
 {
 	Element Elementlist;
 	RootPageBM rootBM;
+	public PlanManager(RootPageBM rBM)
+	{
+		rootBM=rBM;
+	}
+	public Element selectElement(List<Element> ElementList)
+	{
+		
+		Element randomElements=new Element();
+		
+		if (ElementList.size()!=0)
+       	{
+       			int randElement=(int)(Math.random()*ElementList.size());
+       			randomElements=ElementList.get(randElement);
+        }
+		
+		return randomElements;
+	}
 	public Element plan (RootPageBM rootBM)
 	{
-		
+		List<Element> pageElements;
+		pageElements=rootBM.getElements();
+		Element selectedElement=selectElement(pageElements);
+		return selectedElement;
 	}
-	public List<Element> selectElement(ElementList)
-	{
-		
-	}
+	
 }

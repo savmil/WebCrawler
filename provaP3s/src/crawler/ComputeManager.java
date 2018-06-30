d9503fe7906c0c58d6820630549e7874572d21c4
savmil <sav.milo@studenti.unina.it>
2018-06-30 15:02:50 +0200

    caricato selenium

--- provaP3s/src/crawler/ComputeManager.java
+++ provaP3s/src/crawler/ComputeManager.java
@@ -1,13 +1,5 @@
 package crawler;
 
-public class ComputeManager 
-{
-	Error isErrorBM;
-	Error isErrorBUT;
-	VisitedPage resultBM;
-	VisitedPage resultBUT;
-	Report report;
-	Element element;
-	public void computeResult(Element element,Error isErrorBM,Error isErrorBUT, VisitedPage resutltBM, VisitedPage resultBUT, Report report);
-	//per il ragionamento fatto in plan manager si dovrebbe avere una lista di xml
+public class ComputeManager {
+
 }

d05fd45a9620f445ecb2138e03c85b24da8010c2
~Marco <postiglionemarco1@gmail.com>
2018-06-30 17:31:23 +0200

    NavigationStep

--- provaP3s/src/crawler/NavigationStep.java
+++ provaP3s/src/crawler/NavigationStep.java
@@ -1,5 +1,30 @@
 package crawler;
 
 public class NavigationStep {
+	private ErrorP errorBUT;
+	private ErrorP errorBM;
+	private IDelta delta;
+	private VisitedPage rightPage; // Nel caso in cui si verifica un errore, occorre eventualmente salvare la pagina in cui esso non si è verificato.
+	private Element event;
+	
+	public void setErrorBUT(ErrorP e) {
+		this.errorBUT = e;
+	}
+	
+	public void setErrorBM(ErrorP e) {
+		this.errorBM = e;
+	}
+	
+	public void setDelta(IDelta delta) {
+		this.delta = delta;
+	}
+
+	public void setRightPage(VisitedPage page) {
+		this.rightPage = page;
+	}
+
+	public void setEvent(Element event) {
+		this.event = event;
+	}
 
 }

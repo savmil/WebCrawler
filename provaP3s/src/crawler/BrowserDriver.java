3fb4fa59687d658bb179b11c08449590226396ba
valeriolagatta <val.lagatta@studenti.unina.it>
2018-07-01 19:12:47 +0200

    ccdsvcsd

--- provaP3s/src/crawler/BrowserDriver.java
+++ provaP3s/src/crawler/BrowserDriver.java
@@ -1,11 +1,7 @@
 package crawler;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;

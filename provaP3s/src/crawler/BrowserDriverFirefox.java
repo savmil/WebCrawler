f9bfdc5e510d455d2483712db8a677703945066d
valeriolagatta <val.lagatta@studenti.unina.it>
2018-07-03 11:53:46 +0200

    abs su delta + sorpresa!!!

--- provaP3s/src/crawler/BrowserDriverFirefox.java
+++ provaP3s/src/crawler/BrowserDriverFirefox.java
@@ -1,11 +1,17 @@
 package crawler;
 
+
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserDriverFirefox extends BrowserDriver{
 	
 	public BrowserDriverFirefox(){
 		super("webdriver.gecko.driver","selenium\\geckodriver.exe");
-		this.driver = new FirefoxDriver();
+		//this.driver = new FirefoxDriver();
+		
+		FirefoxOptions options = new FirefoxOptions();
+		options.addArguments("--headless");
+		this.driver = new FirefoxDriver(options);
 	}
 }

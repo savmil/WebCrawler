67a1fe00db44614a0fb70b24f9bb5666925f767f
~Marco <postiglionemarco1@gmail.com>
2018-07-03 11:23:34 +0200

    sad

--- provaP3s/src/crawler/BrowserDriverChrome.java
+++ provaP3s/src/crawler/BrowserDriverChrome.java
@@ -7,7 +7,7 @@ public class BrowserDriverChrome extends BrowserDriver {
 	public BrowserDriverChrome()
 	{
 		//super("webdriver.chrome.driver", "C:\\Users\\mario_000\\git\\Psss\\WebCrawler\\WebCrawler\\provaP3s\\selenium\\chromedriver.exe");
-		super("webdriver.chrome.driver","C:\\Users\\Valerio\\git\\WebCrawler\\provaP3s\\selenium\\chromedriver.exe");
+		super("webdriver.chrome.driver","selenium\\chromedriver.exe");
 		//super("webdriver.chrome.driver", "C:\\Users\\posti\\Documents\\GitHub\\WebCrawler\\provaP3s\\selenium\\chromedriver.exe");
 		//super("webdriver.chrome.driver", "C:\\Users\\Saverio\\Desktop\\WebCrawler\\provaP3s\\selenium\\chromedriver.exe");
 		this.driver = new ChromeDriver();

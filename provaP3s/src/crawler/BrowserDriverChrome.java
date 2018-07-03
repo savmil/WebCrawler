7494478631db4c5b39c13bb18bf8a6a2b2dff081
~Marco <postiglionemarco1@gmail.com>
2018-07-03 11:31:53 +0200

    tolti i commenti

--- provaP3s/src/crawler/BrowserDriverChrome.java
+++ provaP3s/src/crawler/BrowserDriverChrome.java
@@ -6,10 +6,7 @@ public class BrowserDriverChrome extends BrowserDriver {
 	
 	public BrowserDriverChrome()
 	{
-		//super("webdriver.chrome.driver", "C:\\Users\\mario_000\\git\\Psss\\WebCrawler\\WebCrawler\\provaP3s\\selenium\\chromedriver.exe");
 		super("webdriver.chrome.driver","selenium\\chromedriver.exe");
-		//super("webdriver.chrome.driver", "C:\\Users\\posti\\Documents\\GitHub\\WebCrawler\\provaP3s\\selenium\\chromedriver.exe");
-		//super("webdriver.chrome.driver", "C:\\Users\\Saverio\\Desktop\\WebCrawler\\provaP3s\\selenium\\chromedriver.exe");
 		this.driver = new ChromeDriver();
 	}
 }
--- provaP3s/src/crawler/BrowserDriverFirefox.java
+++ provaP3s/src/crawler/BrowserDriverFirefox.java
@@ -5,12 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BrowserDriverFirefox extends BrowserDriver{
 	
 	public BrowserDriverFirefox(){
-		
-		//super("webdriver.gecko.driver", "C:\\Users\\mario_000\\git\\Psss\\WebCrawler\\WebCrawler\\provaP3s\\selenium\\geckodriver.exe");
 		super("webdriver.gecko.driver","selenium\\geckodriver.exe");
-		//super("webdriver.gecko.driver","C:\\Users\\Valerio\\git\\WebCrawler\\provaP3s\\selenium\\geckodriver.exe");
-		//super("webdriver.chrome.driver", "C:\\Users\\posti\\Documents\\GitHub\\WebCrawler\\provaP3s\\selenium\\geckodriver.exe");
-		//super("webdriver.gecko.driver", "C:\\Users\\Saverio\\Desktop\\WebCrawler\\provaP3s\\selenium\\geckodriver.exe");
 		this.driver = new FirefoxDriver();
 	}
 }
